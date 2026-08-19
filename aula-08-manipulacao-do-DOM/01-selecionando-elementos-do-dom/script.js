22bf5c759fc9c6d9da8d2ec13f5c7f14e676a92c
ANNE CAROLLINE EVARISTO CANGUSSÚ <anne.cangussu@e239.local>
2026-08-19 14:35:36 -0300

    Exercicio

--- aula-08-manipulacao-do-DOM/01-selecionando-elementos-do-dom/script.js
+++ aula-08-manipulacao-do-DOM/01-selecionando-elementos-do-dom/script.js
@@ -15,4 +15,19 @@ const corpoSite = window.document.body;
 // Mudando a sua cor
 corpoSite.style.background = "#2f2f2f";
 
+// Acessando o conteúdo de um elemento DOM
 document.write(`<br> No 2º parágrafo do site está escrito assim: ${p1.innerText}`);
+
+// Exercicio
+const p3 = document.getElementById("roxo");
+p3.style.backgroundColor = "purple";
+p3.innerText = "Selecionando elementos pelo ID";
+
+// const p4 = document.getElementsByName(azul);
+
+const p5 = document.getElementsByClassName("vermelho");
+p5.innerText = "Selecionando elementos pelo CLASSE";
+p5.style.backgroundColor = "red";
+
+
+
