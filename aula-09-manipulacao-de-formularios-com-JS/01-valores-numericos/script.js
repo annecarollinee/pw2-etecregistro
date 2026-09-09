58869574bc36383c82fc0f8b8de892283cb6c9e5
ANNE CAROLLINE EVARISTO CANGUSSÚ <anne.cangussu@e239.local>
2026-09-09 14:40:40 -0300

    Calculadora

--- aula-09-manipulacao-de-formularios-com-JS/01-valores-numericos/script.js
+++ aula-09-manipulacao-de-formularios-com-JS/01-valores-numericos/script.js
@@ -1,7 +1,7 @@
 // Capturando as inputs
     const inputN1 = document.querySelector('input#n1');
     const inputN2 = document.querySelector('input#n2');
-    const resultado = document.querySelector('div#resultado');
+    const resultado = document.querySelector('input#resultado');
 
 // Funções da calculadora
 function somar(){
@@ -11,5 +11,35 @@ function somar(){
     // Somando os valores
     const soma = n1 + n2;
     // Exibindo o resultado
-    resultado.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${soma}</strong>.`;
+    resultado.value = `${soma}`;
+}
+function subtrair(){
+    // Capturando os valores das inputs
+    const n1 = Number(inputN1.value);
+    const n2 = Number(inputN2.value);
+    // Subtraindo os valores
+    const subtrair = n1 - n2;
+    // Exibindo o resultado
+    resultado.value = `${subtrair}`;
+}
+function multiplicar(){
+    // Capturando os valores das inputs
+    const n1 = Number(inputN1.value);
+    const n2 = Number(inputN2.value);
+    // Multiplicando os valores
+    const multiplicar = n1 * n2;
+    // Exibindo o resultado
+    resultado.value = `${multiplicar}`;
+}
+function dividir(){
+    // Capturando os valores das inputs
+    const n1 = Number(inputN1.value);
+    const n2 = Number(inputN2.value);
+    // Dividindo os valores
+    const dividir = n1 / n2;
+    // Exibindo o resultado
+    resultado.value = `${dividir}`;
+}
+function limpar(){
+    resultado.value = ``;
 }
