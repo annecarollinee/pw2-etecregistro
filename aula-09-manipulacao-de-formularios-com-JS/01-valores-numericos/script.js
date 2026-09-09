744213e75a8e4f77c7ec441355d2211324f7861c
ANNE CAROLLINE EVARISTO CANGUSSÚ <anne.cangussu@e239.local>
2026-09-09 13:51:08 -0300

    Calculadora

--- aula-09-manipulacao-de-formularios-com-JS/01-valores-numericos/script.js
+++ aula-09-manipulacao-de-formularios-com-JS/01-valores-numericos/script.js
@@ -1,14 +1,16 @@
-// Funções da calculadora
-function somar(){
-    // Capturando as inputs
+// Capturando as inputs
+
     const inputN1 = document.querySelector('input#n1');
     const inputN2 = document.querySelector('input#n2');
-    // Capturando os valores da input
+    const resultado = document.querySelector('div#resultado');
+
+// Funções da calculadora
+function somar(){
+    // Capturando os valores das inputs
     const n1 = Number(inputN1.value);
     const n2 = Number(inputN2.value);
     // Somando os valores
     const soma = n1 + n2;
     // Exibindo o resultado
-    const resultado = document.querySelector('div#resultado');
     resultado.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${soma}</strong>.`;
 }
